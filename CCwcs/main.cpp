f831cc35ac72d51d7c75298669d610ff58819e46
Josh <jvcard@iastate.edu>
2016-01-11 17:22:34 -0600

    Added note in main.cpp

--- CCwcs/main.cpp
+++ CCwcs/main.cpp
@@ -9,7 +9,5 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // Note: This is only useful for the sake of testing
 }
