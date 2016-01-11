0482b2af26fee2c1bc40195e93581014bfe648f3
Josh <jvcard@iastate.edu>
2016-01-11 14:26:39 -0600

    Initial Commit

---
+++ CCwcs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CCwcs
+//
+//  Created by Josh Cardenzana on 1/11/16.
+//  Copyright © 2016 JCardenzana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
